4404c736c5254e0a5130fcf107ebc639da245c5c
Lukas Devos <ldevos98@gmail.com>
2025-09-03 16:19:37 -0400

    [Feature] `@names x[1:3]` for constructing arrays of symbols (#106)

--- src/abstractnamedinteger.jl
+++ src/abstractnamedinteger.jl
@@ -48,6 +48,7 @@ name(n::Name) = n.value
 
 """
     @names x y ...
+    @names x[1:3] y[1:3, 2:4] ...
 
 Short-hand notation for constructing "named symbols", i.e. objects that can be used as names.
 In other words, the following expressions are equivalent:
@@ -57,11 +58,25 @@ x, y, z = @names x y z
 x, y, z = Name.((:x, :y, :z))
 ```
 """
-macro names(symbols::Symbol...)
-  if length(symbols) == 1
-    return :($(Name)($(QuoteNode(only(symbols)))))
+macro names(exs...)
+  length(exs) == 1 && return esc(:($(_parse_name(only(exs)))))
+  syms_exs = map(_parse_name, exs)
+  return esc(:(($(syms_exs...),)))
+end
+
+_parse_name(ex::Symbol) = :($(Name(ex)))
+function _parse_name(ex)
+  Meta.isexpr(ex, :ref) || throw(ArgumentError("invalid @names expression: $ex"))
+  length(ex.args) > 1 ||
+    throw(ArgumentError("@names indexing expression requires at least one set of indices"))
+  sym = QuoteNode(first(ex.args))
+  if length(ex.args) == 2
+    return :([$Name(Symbol($sym, :_, x)) for x in $(ex.args[2])])
   else
-    return :($(Name).($symbols))
+    return :([
+      $Name(Symbol($sym, Iterators.flatmap(y -> (:_, y), x)...)) for
+      x in Iterators.product($(ex.args[2:end]...))
+    ])
   end
 end
 
