0ea17bb58921501d25d7488e29f23013eb52eb59
Matt Fishman <mtfishman@users.noreply.github.com>
2025-12-10 21:59:23 -0500

    Delete GradedArraysExt (#136)

--- test/test_blockarraysext.jl
+++ test/test_blockarraysext.jl
@@ -1,14 +1,15 @@
-using BlockArrays: Block
-using BlockSparseArrays: BlockSparseArray
+using BlockArrays: Block, BlockArray
 using NamedDimsArrays: dename, nameddims, inds
 using Test: @test, @testset
 
 @testset "NamedDimsArraysBlockArraysExt" begin
     elt = Float64
 
-    a = BlockSparseArray{elt}(undef, [2, 3], [2, 3])
+    a = BlockArray{elt}(undef, [2, 3], [2, 3])
+    a[Block(1, 1)] = randn(elt, 2, 2)
     a[Block(2, 1)] = randn(elt, 3, 2)
     a[Block(1, 2)] = randn(elt, 2, 3)
+    a[Block(2, 2)] = randn(elt, 3, 3)
     n = nameddims(a, ("i", "j"))
     i, j = inds(n)
     @test dename(n[i[Block(2)], j[Block(1)]]) == a[Block(2, 1)]
@@ -20,12 +21,16 @@ using Test: @test, @testset
     @test dename(n[Block.(1:2), Block(1)]) == a[Block.(1:2), Block(1)]
     @test dename(n[Block.(1:2), Block.(1:2)]) == a[Block.(1:2), Block.(1:2)]
 
-    a = BlockSparseArray{elt}(undef, [2, 3], [2, 3])
+    a = BlockArray{elt}(undef, [2, 3], [2, 3])
+    a[Block(1, 1)] = randn(elt, 2, 2)
     a[Block(2, 1)] = randn(elt, 3, 2)
     a[Block(1, 2)] = randn(elt, 2, 3)
-    b = BlockSparseArray{elt}(undef, [2, 3], [2, 3])
+    a[Block(2, 2)] = randn(elt, 3, 3)
+    b = BlockArray{elt}(undef, [2, 3], [2, 3])
+    b[Block(1, 1)] = randn(elt, 2, 2)
     b[Block(2, 1)] = randn(elt, 3, 2)
     b[Block(1, 2)] = randn(elt, 2, 3)
+    b[Block(2, 2)] = randn(elt, 3, 3)
     na = nameddims(a, ("i", "j"))
     nb = nameddims(b, ("j", "i"))
     nc = na .+ 2 .* nb
